c32dc2bb0a25cb92d093e5f75da00263014cbf1c
Cody Burrows <code1010@rochester.rr.com>
2015-05-11 17:40:53 -0400

    Border tweaks

--- UDP-local/src/Window.java
+++ UDP-local/src/Window.java
@@ -18,23 +18,11 @@ import javax.swing.JPanel;
 
 public class Window extends JPanel {
 
-	int x = 100;
-	int y = 100;
-	int deltaX = 1;
-	int deltaY = 1;
-	
-	public void move(){
-			
-		x += deltaX;
-		y += deltaY;
-		
-	}
-	
 	public static void main(String [] args) throws InterruptedException{
-		JFrame frame = new JFrame("Testing 123");
+		JFrame frame = new JFrame("Wire Warfare");
 		Window game = new Window();
 		frame.add(game);
-		frame.setSize(930, 600);
+		frame.setSize(910, 550);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		frame.setResizable(false);
@@ -48,11 +36,11 @@ public class Window extends JPanel {
 		Graphics2D gd = (Graphics2D) g;
 		gd.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
 		gd.setColor(Color.GRAY);
-		gd.fillRect(0, 0, 930, 600);
+		gd.fillRect(0, 0, 910, 590);
 		try {
 			BufferedImage img;
 			img = ImageIO.read(new File("src/border.png"));
-			gd.drawImage(img, -7, -7, null);
+			gd.drawImage(img, -17, -17, null);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
